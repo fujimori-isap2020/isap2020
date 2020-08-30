4da7613c832e90562d5d03bf7db3e8ffad021311
sekihanium <26105214+sekihanium@users.noreply.github.com>
2020-08-30 13:51:11 +0900

    ひとまずそれらしい値が出るようになった

---
+++ 5_8GHz.py
@@ -0,0 +1,123 @@
+# this program is made for sdc in isap2020
+# transmitter A: 300-320MHz, -50dBm, lambda/2 dipole, horizontal, 1-3antennas, CW
+# transmitter B: 2.402-2.480GHz, 0dBm, Small antenna, unknown pole, 1-3antennas, ble beacon
+# transmitter C: 5.012-5.025GHz, 10dBm, Monopole antenna, Vertical, 1-2, CW
+#
+
+import numpy as np
+import pandas as pd
+from math import atan2
+
+
+class Antenna(object):
+    def __init__(self, x, y, freq, power, orientation, phase_offset):
+        self.x_axis = x
+        self.y_axis = y
+        self.freq = freq
+        self.power = power
+        self.orientation = orientation
+        c = 3.0e8
+        self.lambda_0 = c / freq
+        self.phase_offset = phase_offset
+
+        # todo
+        self.gain = 0
+
+    def get_gain_by_angle(self, ant):
+        return 1
+
+    def get_distance_from(self, ant):
+        x_diff = ant.x_axis - self.x_axis
+        y_diff = ant.y_axis - self.y_axis
+        return np.sqrt(x_diff**2 + y_diff**2)
+
+    def get_distance_by_points(self, points):
+        x_diff = points[:, 0] - self.x_axis
+        y_diff = points[:, 1] - self.y_axis
+        return np.sqrt(x_diff**2 + y_diff**2)
+
+    def get_phase_from_distance(self, distance):
+        return (distance % self.lambda_0) / self.lambda_0 * (2 * np.pi) + self.phase_offset
+
+    def get_att_by_points(self, points):
+        target_angles = atan2(points.x - self.x_axis, points.y - self.y_axis)
+        pass
+
+    def get_angles_by_points(self, points):
+        pass
+
+    def get_amplitude_by_points(self, points):
+        pass
+
+    def get_phase_by_ants(self, ants):
+        distances = self.get_distance_from(ants)
+        phases = self.get_phase_from_distance(distances)
+        return phases
+
+
+def received_power_by_friis(power_tx, gain_tx, gain_rx, distance, lambda_0):
+    # 自由空間のゲイン
+    gf = -10 * np.log10((4 * np.pi * distance / lambda_0) ** 2) + (gain_tx + gain_rx)
+    received_power = power_tx + gf
+    return received_power
+
+
+def dbm_to_v(power_dbm, impedance):
+    dbuV = power_dbm + 10 * np.log10(impedance) + 90
+    voltage = (10 ** (dbuV/20)) / 1e6
+    return voltage
+
+
+def calc_two_antennas(x1, y1, x2, y2, phase_diff):
+    power_tx = 10
+    gain_tx = 0
+    # 受信側はダイポールで設定？？
+    ant1 = Antenna(x1, y1, 5.8e9, 10, 90, 0)
+    ant2 = Antenna(x2, y2, 5.8e9, 10, 90, phase_diff)
+    tx_ants = [ant1, ant2]
+
+    gain_rx = 0
+    rx_ants = np.array([[0, -1], [0.5, -1], [1, -1], [1.5, -1], [2, -1], [2.5, -1], [3, -1]])
+
+    complex_signals = list()
+    for ant in tx_ants:
+        # 常に各変数は受信アンテナ分のデータが入った配列になっている
+        # 距離の取得
+        distances = ant.get_distance_by_points(rx_ants)
+        # 距離を使って受信点での位相の計算．
+        phases = ant.get_phase_from_distance(distances)
+        print(max(phases/(2*np.pi)*360), min(phases/(2*np.pi)*360))
+        # フリスの公式を使って受信点でのパワーを計算．
+        power_rxs = received_power_by_friis(ant.power, ant.gain, gain_rx, distances, ant.lambda_0)
+        # 振幅に変換
+        amp_rxs = dbm_to_v(power_rxs, 50)
+        # 振幅と位相から複素信号を計算
+        complex_signal = np.sqrt(amp_rxs) * np.exp(1j * phases)
+        complex_signals.append(complex_signal)
+
+    # 受信アンテナごとに重ね合わせ
+    sum_signals = np.sum(complex_signals, 0)
+    return sum_signals
+
+
+def plotter(test):
+    import matplotlib.pyplot as plt
+    for i in range(10):
+        plt.plot(abs(test[i]), label=f'{i}')
+    plt.title('ant1=(0, 0.0-0.1), ant2=(1.2, 1.5)')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    test = list()
+    for x1 in range(0, 300, 10):
+        for y1 in range(0, 300, 10):
+            test.append(calc_two_antennas(x1/100, y1/100, 1.2, 1.5, 0))
+
+    #print(abs(np.array(test)))
+    plotter(test)
+
+
+
+
