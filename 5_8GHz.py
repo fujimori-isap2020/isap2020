70b8c1dc2f1197d284159dee9d488e6118b05e33
sekihanium <26105214+sekihanium@users.noreply.github.com>
2020-09-08 14:52:30 +0900

    送信側アンテナを合成する際にnumpyを使うように変更．

--- 5_8GHz.py
+++ 5_8GHz.py
@@ -57,7 +57,8 @@ class Antennas(object):
         x_diff = x_diff[:, :, np.newaxis]
         y_diff = y_diff[:, np.newaxis, :]
         # 単位はメートルで返す．
-        return np.sqrt(x_diff**2 + y_diff**2) / 100
+        distances = np.sqrt(x_diff**2 + y_diff**2) / 100
+        return distances.reshape(7, 900)
 
     def get_phase_from_distance(self, distance):
         # returns (900, 7)
@@ -89,43 +90,6 @@ def v_to_dbm(voltage, impedance):
     return dbm
 
 
-def calc_two_antennas_vector(phase_diff):
-    x1_grid = np.arange(0, 300, 10)
-    y1_grid = np.arange(0, 300, 10)
-    x2_grid = np.arange(0, 300, 10)
-    y2_grid = np.arange(0, 300, 10)
-
-    # 受信側はダイポールで設定？？
-    ant1 = Antennas(x1_grid, y1_grid, 5.8e9, 10, 90, 0)             # Antennasはx,yのgridに対してとりうる全ての点を保持する．
-    ant2 = Antennas(x2_grid, y2_grid, 5.8e9, 10, 90, phase_diff)
-    tx_ants = [ant1, ant2]
-
-    gain_rx = 0
-    rx_ants = np.array([[0, -1], [0.5, -1], [1, -1], [1.5, -1], [2, -1], [2.5, -1], [3, -1]])
-
-    complex_signals = list()
-    for ant in tx_ants:
-        # ７個の各アンテナからantのとりうる位置30x30点に対する距離の取得 (distances.shape = (900, 7))
-        distances = ant.get_distance_by_points(rx_ants)
-        # 距離を使って受信点での位相の計算．(phases.shape = (900, 7))
-        phases = ant.get_phase_from_distance(distances)
-        #print(max(phases/(2*np.pi)*360), min(phases/(2*np.pi)*360))
-        # フリスの公式を使って受信点でのパワーを計算．
-        # 距離を使って受信点での位相の計算．(power_rxs.shape = (900, 7))
-        power_rxs = received_power_by_friis(ant.power, ant.gain, gain_rx, distances, ant.lambda_0)
-        # 振幅に変換 (power_rxs.shape = (900, 7))
-        amp_rxs = dbm_to_v(power_rxs, 50)
-        # 振幅と位相から複素信号を計算 (power_rxs.shape= (900, 7))
-        complex_signal = np.sqrt(amp_rxs) * np.exp(1j * phases)
-        complex_signals.append(complex_signal)
-
-    # complex_signals.shape -> (2, 900, 7)
-    complex_signals = np.array(complex_signals)
-    # 受信アンテナごとに重ね合わせ (complex_signals[0] ? complex_signals[1]).shape -> (810000, 7)
-    sum_signals = np.sum(complex_signals, 0)
-    return sum_signals
-
-
 def plotter(test):
     plt.figure(dpi=200, figsize=(5, 5))
     plt.rcParams['xtick.direction'] = 'in'
@@ -149,12 +113,55 @@ def plotter(test):
     plt.show()
 
 
+def calc_two_antennas_vector(phase_diff):
+    x1_grid = np.arange(0, 300, 10)
+    y1_grid = np.arange(0, 300, 10)
+    x2_grid = np.arange(0, 300, 10)
+    y2_grid = np.arange(0, 300, 10)
+
+    # 受信側は等方性で仮定
+    # Antennasはx,yのgridに対してとりうる全ての点を保持する．
+    ant1 = Antennas(x1_grid, y1_grid, 5.8e9, 10, 90, 0)
+    ant2 = Antennas(x2_grid, y2_grid, 5.8e9, 10, 90, phase_diff)
+    tx_ants = [ant1, ant2]
+
+    gain_rx = 0
+    rx_ants = np.array([[0, -1], [0.5, -1], [1, -1], [1.5, -1], [2, -1], [2.5, -1], [3, -1]])
+
+    complex_signals = list()
+    for ant in tx_ants:
+        # ７個の各アンテナからantのとりうる位置30x30点に対する距離の取得 (distances.shape = (900, 7))
+        distances = ant.get_distance_by_points(rx_ants)
+        print(f'distances.shape: {distances.shape}')
+        # 距離を使って受信点での位相の計算．(phases.shape = (900, 7))
+        phases = ant.get_phase_from_distance(distances)
+        print(f'phases.shape: {phases.shape}')
+        #print(max(phases/(2*np.pi)*360), min(phases/(2*np.pi)*360))
+        # フリスの公式を使って受信点でのパワーを計算．
+        # 距離を使って受信点での位相の計算．(power_rxs.shape = (900, 7))
+        power_rxs = received_power_by_friis(ant.power, ant.gain, gain_rx, distances, ant.lambda_0)
+        print(f'power_rxs.shape: {power_rxs.shape}')
+        # 振幅に変換 (power_rxs.shape = (900, 7))
+        amp_rxs = dbm_to_v(power_rxs, 50)
+        print(f'amp_rxs.shape: {power_rxs.shape}')
+        # 振幅と位相から複素信号を計算 (power_rxs.shape= (900, 7))
+        complex_signal = np.sqrt(amp_rxs) * np.exp(1j * phases)
+        complex_signals.append(complex_signal)
+
+    # complex_signals.shape -> (2, 7, 900)
+    complex_signals = np.array(complex_signals)
+    print(f'comprex_signals.shape: {complex_signals.shape}')
+    # 受信アンテナごとに重ね合わせ (complex_signals[0] ? complex_signals[1]).shape -> (810000, 7)
+    #sum_signals = np.sum(complex_signals, 0)
+    sum_signals_for_each_rx = complex_signals[0][:, :, None] + complex_signals[1][:, None, :]
+    return sum_signals_for_each_rx
+
 if __name__ == '__main__':
     deg_start = sys.argv[1]
     deg_stop = sys.argv[2]
     print(f'processing {deg_start} from {deg_stop}')
     for deg in tqdm(range(int(deg_start), int(deg_stop), 1)):
-        calc_two_antennas_vector(deg)
+        print(calc_two_antennas_vector(deg).shape)
 
 
 
