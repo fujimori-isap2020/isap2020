1317cdc0714de3315e5605c0832762bda4d7fba5
kensuke kobayashi <25693250+jeffy890@users.noreply.github.com>
2020-09-21 08:22:49 +0900

    shuffle and split
    shuffle and split by numpy
    output train and test csv

--- csvpy.py
+++ csvpy.py
@@ -5,10 +5,15 @@ result = (np.load('result.npy')).reshape(7,810000).T
 positions = (np.load('positions.npy')).reshape(4,810000).T
 
 dataset = np.concatenate([result, positions], 1)
-#dataset = dataset[:3]
-dataset = pd.DataFrame(dataset)
-dataset.columns = ['x__0', 'x__1', 'x__2', 'x__3', 'x__4', 'x__5', 'x__6', 'y__0', 'y__1', 'y__2', 'y__3']
-print(dataset.info())
+np.random.shuffle(dataset)
 
-dataset.to_csv('dataset.csv', index=False)
-#np.savetxt('dataset.csv', dataset, delimiter=',')
+train, test = np.split(dataset, [int(810000 * 0.8)])
+
+train = pd.DataFrame(train)
+test = pd.DataFrame(test)
+
+train.columns = ['x__0', 'x__1', 'x__2', 'x__3', 'x__4', 'x__5', 'x__6', 'y__0', 'y__1', 'y__2', 'y__3']
+test.columns = ['x__0', 'x__1', 'x__2', 'x__3', 'x__4', 'x__5', 'x__6', 'y__0', 'y__1', 'y__2', 'y__3']
+
+train.to_csv('train.csv', index=False)
+test.to_csv('test.csv', index=False)
